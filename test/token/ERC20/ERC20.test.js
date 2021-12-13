6b62d730023e1a375d408c0ec69180e9cbfe45c3
iam-dev <elky.bachtiar@duo.nl>
2021-12-13 22:25:15 +0100

    remove Greeter

--- test/token/ERC20/ERC20.test.js
+++ test/token/ERC20/ERC20.test.js
@@ -195,39 +195,7 @@ contract('ERC20', function (accounts) {
     });
   });
 
-  // describe('_mint', function () {
-  //   const amount = new BN(50);
-  //   it('rejects a null account', async function () {
-  //     await expectRevert(
-  //       this.token.mint(ZERO_ADDRESS, amount), 'ERC20: mint to the zero address',
-  //     );
-  //   });
-
-  //   describe('for a non zero account', function () {
-  //     beforeEach('minting', async function () {
-  //       const { logs } = await this.token.mint(recipient, amount);
-  //       this.logs = logs;
-  //     });
-
-  //     it('increments totalSupply', async function () {
-  //       const expectedSupply = initialSupply.add(amount);
-  //       expect(await this.token.totalSupply()).to.be.bignumber.equal(expectedSupply);
-  //     });
-
-  //     it('increments recipient balance', async function () {
-  //       expect(await this.token.balanceOf(recipient)).to.be.bignumber.equal(amount);
-  //     });
-
-  //     it('emits Transfer event', async function () {
-  //       const event = expectEvent.inLogs(this.logs, 'Transfer', {
-  //         from: ZERO_ADDRESS,
-  //         to: recipient,
-  //       });
-
-  //       expect(event.args.value).to.be.bignumber.equal(amount);
-  //     });
-  //   });
-  // });
+  //
 
   // describe('_burn', function () {
   //   it('rejects a null account', async function () {
