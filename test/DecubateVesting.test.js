e72ee6df8001d3243adb6d35e85e1cb47f02127f
iam-dev <elky.bachtiar@duo.nl>
2021-12-14 10:22:44 +0100

    addVestingStrategy reverted strategy

--- test/DecubateVesting.test.js
+++ test/DecubateVesting.test.js
@@ -1,12 +1,13 @@
-const { BN, constants, expectEvent, expectRevert } = require('@openzeppelin/test-helpers');
+const { BN, constants, time } = require('@openzeppelin/test-helpers');
 const { expect } = require('chai');
+const { ethers } = require("hardhat");
 const { ZERO_ADDRESS } = constants;
 
 const DecubateERC20Whitelisted = artifacts.require('DecubateERC20Whitelisted');
 const DecubateVesting = artifacts.require('DecubateVesting');
 
 contract('DecubateVesting', function (accounts) {
-  const [ initialHolder ] = accounts;
+  const [ initialHolder, anotherAccount ] = accounts;
 
   const name = 'My Token';
   const symbol = 'MTKN';
@@ -20,5 +21,24 @@ contract('DecubateVesting', function (accounts) {
 
   it('has a token', async function () {
     expect(await this.vesting.getToken()).to.equal(this.token.address);
+  });
+
+  it('change a token', async function () {
+    await this.vesting.setToken(ZERO_ADDRESS);
+    expect(await this.vesting.getToken()).to.equal(ZERO_ADDRESS);
+  });
+
+  context('addVestingStrategy', async function () {
+    it('should revert non owner', async function () {
+      await expect(this.vesting.addVestingStrategy(
+        'Reverted Strategy',
+        9327600,
+        await time.latest(),
+        await time.duration.minutes(10),
+        100,
+        false,
+        { from: this.anotherAccount }
+      )).to.be.revertedWith('Test');
+    });
   });
 });
