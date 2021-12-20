f131267ea2d859edfa84580795734425f23b5755
iam-dev <elky.bachtiar@duo.nl>
2021-12-20 10:57:11 +0100

    latest changes

--- contracts/BithotelPair.sol
+++ contracts/BithotelPair.sol
@@ -11,7 +11,7 @@ contract BithotelPair {
     address private constant BUSD = 0x8301F2213c0eeD49a7E28Ae4c3e91722919B8B47;//BUSD TESTNET 0x8301f2213c0eed49a7e28ae4c3e91722919b8b47
 
     //IUniswapV2Router02 private constant _ROUTER = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E); //Pancakeswap MAINNET
-    IUniswapV2Router02 private constant _ROUTER = IUniswapV2Router02(0xD99D1c33F9fC3444f8101754aBC46c52416550D1); //Pancakeswap TESTNET
+    IUniswapV2Router02 private constant _ROUTER = IUniswapV2Router02(0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3); //Pancakeswap TESTNET
     address private _routerAddress;
     IUniswapV2Pair private _pair;
     address private _pairAddress;
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -49,7 +49,7 @@ async function main() {
   if (hre.network.name !== 'binance') {
     await token.whiteList('0x10ED43C718714eb63d5aA57B78B54704E256024E', true);
   } else if (hre.network.name !== 'binanceTesnet') {
-    await token.whiteList('0xD99D1c33F9fC3444f8101754aBC46c52416550D1', true);
+    await token.whiteList('0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3', true);
   }
 
   // eslint-disable-next-line max-len
